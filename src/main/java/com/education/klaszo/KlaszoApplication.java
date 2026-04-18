fa6af70da9ded5fc61f760996b43d7e5187ff657
Shubham Singh <shubhamsingh43231@gmail.com>
2026-04-18 15:39:34 +0530

    🙏  Jai Shree Ganesh 🙏

---
+++ src/main/java/com/education/klaszo/KlaszoApplication.java
@@ -0,0 +1,13 @@
+package com.education.klaszo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlaszoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlaszoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/education/klaszo/KlaszoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.klaszo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlaszoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
